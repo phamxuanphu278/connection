5649e3734f9b8ef3f80525b0dda3fd80b062fdda
phamxuanphu278 <phuphamxuan278@gmail.com>
2019-12-25 16:35:55 +0700

    up

--- js/script.js
+++ js/script.js
@@ -2,30 +2,52 @@ function getRandomArbitrary(min, max){
 	return Math.floor(Math.random() * (max - min)) + min;
 }
 
+function get_random_color() 
+{
+    var color = Math.floor(Math.random() * Math.pow(256, 3)).toString(16);
+    while(color.length < 6) {
+        color = "0" + color;
+    }
+    return "#" + color;
+}
+
+
 $(document).ready(function(){
 
 	$(window).on('load', function () {
     if ($('#preloader').length) {
-      $('#preloader').delay(2200).fadeOut('slow', function () {
+      $('#preloader').delay(1).fadeOut('slow', function () {
         $(this).remove();
       });
     }
   });
 
+	if ($('.text-slider').length == 1) {
+    var typed_strings = $('.text-slider-items').text();
+		var typed = new Typed('.text-slider', {
+			strings: typed_strings.split(','),
+			typeSpeed: 80,
+			loop: true,
+			backDelay: 1100,
+			backSpeed: 30
+		});
+	}
+	
 	setInterval(function(){
 		var screenHeight = $(document).height();
 		var screenWidth = $(document).width();
 		var startLeft = getRandomArbitrary(0, screenWidth);
 		var timeRun = getRandomArbitrary(4000, 6000);
 		var opacityR = Math.random() * (1 - 0.2) + 0.2;
-		var sizeR = getRandomArbitrary(15, 40);
+		var sizeR = getRandomArbitrary(30, 40);
 		var endLeft = getRandomArbitrary(startLeft - 100, startLeft + 100);
 		var snow = document.createElement('span');
+		var colorS = get_random_color();
 
-		$(snow).addClass('snow-item fa fa-heart').css({
+		$(snow).addClass('snow-item fa fa-gift').css({
 			'position'  : 'absolute',
 			'z-index'   : 'auto',
-			'color'     : '#ff0000',
+			'color'     : colorS,
 			'display'   : 'block',
 			'top'       : 0,
 			'left'      : startLeft,
